baaf21c777ca1a9226d8635be21e6475209c56b5
RafaDom10 <r-domingues@outlook.com>
2020-10-15 14:56:55 -0300

    Backend and web finished.

---
+++ backend/src/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
+import OrpanagesController from './controllers/OrphanagesController';
+
+
+const routes = Router();
+const upload = multer(uploadConfig);
+
+routes.get('/orphanages', OrpanagesController.index);
+routes.get('/orphanages/:id', OrpanagesController.show);
+routes.post('/orphanages', upload.array('images'), OrpanagesController.create);
+
+export default routes;
